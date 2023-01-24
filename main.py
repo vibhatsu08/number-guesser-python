2b542339f158ec6ea203b6f8987f93e4509a60f2
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-24 06:26:27 +0530

    V1.2 of the number guessing game is ready for testing.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 
 # The random module to import the random function.
 import random
+# importing the time module, to add a certain time delay for some flair.
+import time
 
 # Starts the game with the input response.
 def startGame () :
@@ -12,6 +14,7 @@ def startGame () :
     response = input("---> Do you want to play the number guesser game...Enter yes or no to get started : ")
     # If the reponse from the user is yes.
     if response.lower() == "yes" :
+        timeDelay ()
         print("-> Response was : yes")
         runningProgramStatement ()
         chooseNumber ()
@@ -25,14 +28,19 @@ def startGame () :
 def chooseNumber () :
     print("-> The Computer has chosen the number...")
     numberChosen = random.randint(0, 100)
+    timeDelay ()
     runningProgramStatement ()
     initializeGame (numberChosen)
 
 def initializeGame (numberChosen) :
     # TBD - convert these print statements into an array of strings to be looped and printed.
+    timeDelay ()
     print("-> Your task is to guess the number chosen by the computer...")
+    timeDelay ()
     print("-> You will be provided with 10 tries...")
+    timeDelay ()
     print("-> With every unsuccessful attempt, the computer will provide a hint to you...")
+    timeDelay ()
     print("-> Good luck!")
     
     # Number of attempts for the user, starts at 0, will keep incrementing by 1.
@@ -40,24 +48,29 @@ def initializeGame (numberChosen) :
     runningProgramStatement ()
     print("---> Guess your number : ")
     guessNumberGame (attempts, numberChosen)
+    timeDelay ()
     
 # Function to guess the number chosen by the computer
 def guessNumberGame (attempts, numberChosen) :
     guessNumber = int(input())
     if guessNumber > 100 or guessNumber < 0 :
+        timeDelay ()
         print("...C'mon, be realistic, you need to choose a number between 0 and 100...")
         attempts += 1
         guessNumberGame (attempts, numberChosen)
     if guessNumber > numberChosen :
+        timeDelay ()
         print("...Try lower...")
         attempts += 1
         guessNumberGame (attempts, numberChosen)
     if guessNumber < numberChosen :
+        timeDelay ()
         print("...Try higher...")
         attempts += 1
         guessNumberGame (attempts, numberChosen)
         
     if guessNumber == numberChosen :
+        timeDelay ()
         gameOver (attempts, numberChosen)
 
 # Function for the final part of the game.
@@ -68,6 +81,10 @@ def gameOver (attempts, numberChosen) :
 # Function for the running program statement
 def runningProgramStatement () :
     print("### RUNNING PROGRAM ###")
+    timeDelay ()
+    
+def timeDelay () :
+    time.sleep(2)
     
 # Takes the input response.
 print(startGame ())
